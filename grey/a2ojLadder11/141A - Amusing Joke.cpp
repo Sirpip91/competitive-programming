962e160b0d43318b1d210c5836570e29d9de833e
Bradley Allen <allenbradley91@hotmail.com>
2022-12-30 11:51:43 -0600

    Update 141A - Amusing Joke.cpp

--- grey/a2ojLadder11/141A - Amusing Joke.cpp
+++ grey/a2ojLadder11/141A - Amusing Joke.cpp
@@ -3,7 +3,7 @@
 using namespace std;
  
  
- 
+ //my braindead solution lol
  
 int main()
 {	
@@ -42,3 +42,40 @@ int main()
 
 }
 
+
+//simple solution 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+	
+	//input threel ines 
+	string gname;
+	string hname;
+	string pile;
+	string no = "NO";
+	string yes = "YES";
+ 
+	
+	//get inputs
+	cin >> gname >> hname >> pile;
+ 
+	//add the two names into one string s
+	string  s = gname + hname;
+ 
+	//sort the string s
+	sort(s.begin(), s.end());
+	//sort the pile.
+	sort(pile.begin(), pile.end());
+ 
+	//if the pile and the string are equal then yes
+	if(s == pile)
+		cout << yes << endl;
+	else
+		cout << no << endl;
+ 
+	
+	return 0;	
+}
