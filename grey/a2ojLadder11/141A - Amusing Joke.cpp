2de578e8c0721253bc7cc6bb0ba8831ff7d4f97b
Bradley Allen <allenbradley91@hotmail.com>
2022-12-30 11:50:30 -0600

    Create 141A - Amusing Joke.cpp

---
+++ grey/a2ojLadder11/141A - Amusing Joke.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+ 
+int main()
+{	
+
+	
+	unordered_map<char,int>mp;
+	unordered_map<char,int>np;
+	string s1,s2,s3;
+
+	cin >> s1 >> s2 >> s3;
+	int one = s1.size();
+	int two = s2.size();
+	int three = s3.size();
+
+	for(int i = 0;i<one;i++)
+	{
+		mp[s1[i]]++;
+	}
+	for(int i = 0; i<two;i++)
+	{
+		mp[s2[i]]++;
+	}
+
+	for(int i =0; i<three;i++)
+	{
+		np[s3[i]]++;
+	}
+
+	if(mp == np)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+
+	return 0;
+
+}
+
